c345d3b3bb4a292433583d14b7c4923cadfb83d0
Laura <laurapasinis@gmail.com>
2022-02-18 10:08:26 -0400

    exercicio 4

---
+++ app.js
@@ -0,0 +1,6 @@
+let paises = [];
+
+paises.push ('Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa');
+
+console.table(paises);
+
